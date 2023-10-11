914a20e072e26e5e789f7504ff595fbbb510ed7a
BuiDuyHung <hungb.z98@gmail.com>
2023-10-11 11:10:28 +0700

    fix scale camera

---
+++ Assets/Scripts/Camera/CameraScaler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScaler : MonoBehaviour
+{
+    private float targetAspectRatio = 1920f / 1080f;
+    new private Camera camera;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+        camera.aspect = targetAspectRatio;
+    }
+}
--- Assets/Scripts/Item/Hook/DropHook.cs
+++ Assets/Scripts/Item/Hook/DropHook.cs
@@ -45,8 +45,6 @@ public class DropHook : GameController
 
     }
 
-    
-
     public override IEnumerator MovePosition()
     {
         isMoving = true;
